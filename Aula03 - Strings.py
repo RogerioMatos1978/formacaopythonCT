f89b9c7cf1636a7529218b1791fda023b5b43040
RogerioMatos1978 <rs_matos78@outlook.com>
2025-05-20 08:00:00 -0300

    Update Aula03 - Strings.py

--- Aula03 - Strings.py
+++ Aula03 - Strings.py
@@ -13,23 +13,4 @@ nomes = ["Rogerio", "Alicya" , "Maria", "Matos", "Santos"]
 for i in sorted(nomes):
     print(i)
 
-def Letras(letras):
-    s = 'enter'
-    while s != "":
-        s = input("Letra: ")
-        if s != "":
-            ch = s[0]
-            letras.append(ch)
-    return
-def Inverte(letras, inverso):
-    n = len(letras)
-    for i in range(n-1,-1,-1):
-        inverso.append(letras[i])
-    return
 
-letras = []
-inverso = []
-Letras(letras)
-print(letras)
-Inverte(letras, inverso)
-print(inverso)
