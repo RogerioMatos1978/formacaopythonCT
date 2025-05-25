a9895f8f1a0c67182c3281545220b9641b6dd90c
Rogerio Matos <rs_matos78@outlook.com>
2025-05-25 15:27:00 -0300

    Exercicios01 Lista alguns resolvidos

--- Exercicios01.py
+++ Exercicios01.py
@@ -1,28 +1,41 @@
 # Exercício #001
 # Crie um programa que pergunte o nome e a idade do usuário e
 # mostre a seguinte mensagem: "Seu nome é {nome} e você tem {idade} anos"
+import math as m
 
-
+nome = input("Informe o teu Nome: ")
+idade = int(input("Informe a tua idade: "))
+print(f"O seu nome é {nome}, e sua idade é {idade}.")
 
 # Exercício #002
 # Crie um programa que peça um número inteiro e um número float e exiba a soma dos dois.
-
-
+numero_float = float(input("Informe um numero Float: "))
+numero_inteiro = int(input("Informe um numero Inteiro: "))
+soma_numeros = numero_float + numero_inteiro
+print(f"A soma do numero float: {numero_float} + o numero inteiro: {numero_inteiro}, é: ", soma_numeros)
 
 # Exercício #003
 # Crie um programa que solicite dois números float e calcule a média entre eles.
-
-
+numero_float_01 = float(input("Digite o primeiro número Float: "))
+numero_float_02 = float(input("Digite o segundo número Float: "))
+media_dois_float = (numero_float_01 + numero_float_02) / 2
+print(f"A média entre os dois numeros informados é: ", media_dois_float)
 
 # Exercício #004
 # Crie um programa que peça uma frase e informe quantos caracteres ela possui.
-
-
+frase = input("Digite uma frase para descobrir quantos caracteres tem: ")
+tamanho_frase = len(frase)
+print(f"O total de caracteres da frase é {tamanho_frase}.")
 
 # Exercício #005
 # Crie um programa que solicite um número inteiro e mostre o dobro, o triplo e a raiz quadrada desse número.
-
-
+numero_inteiro_ = int(input("Informe um numero inteiro: "))
+dobro = numero_inteiro_ * 2
+print(f"O dobro do numero {numero_inteiro_}informado é {dobro}:")
+triplo = numero_inteiro_ * 3
+print(f"O triplo do numero {numero_inteiro_} informado é {triplo}.")
+raiz_quadrada = m.sqrt(numero_inteiro_)
+print(f"O raiz quadrada do numero {numero_inteiro_} informado é {raiz_quadrada:.2f}.")
 
 # Exercício #006
 # Crie um programa que peça a largura e o comprimento de um terreno e calcule sua área.
