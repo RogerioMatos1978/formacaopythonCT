1c6fafcc0700eef79f5371eacfc86ed214b9fd09
Rogerio Matos <rs_matos78@outlook.com>
2025-05-25 09:18:09 -0300

    Zen of Python

---
+++ Aula00 - Zen of Python.py
@@ -0,0 +1,30 @@
+# Zen Of Python
+
+# Filosofia do Programador Python (Zen of Python)
+# Abaixo estão alguns princípios da filosofia do Python, também conhecidos como Zen of Python.
+# Eles trazem dicas de boas práticas e valores que guiam a forma de desenvolver em Python.
+
+# Bonito é melhor do que feio.
+# Explícito é melhor do que implícito.
+# Simples é melhor do que complexo.
+# Complexo é melhor do que complicado.
+# Linear é melhor do que aninhado.
+# Esparso é melhor que denso.
+# A legibilidade importa.
+# Casos especiais não são especiais o bastante para quebrar as regras.
+# Embora a praticidade supere a pureza.
+# Erros nunca devem passar silenciosamente.
+# A menos que sejam explicitamente silenciados.
+# Diante da ambiguidade, recuse a tentação de adivinhar.
+# Deve haver uma — e de preferência apenas uma — maneira óbvia de fazê-lo.
+# Embora essa maneira possa não ser óbvia à primeira vista, a menos que você seja holandês.
+# Agora é melhor do que nunca.
+# Embora 'nunca' muitas vezes seja melhor do que agora mesmo.
+# Se a implementação é difícil de explicar, então é uma má ideia.
+# Se a implementação é fácil de explicar, então pode ser uma boa ideia.
+# Namespaces são uma ideia sensacional — vamos usar mais deles!
+
+# Easter Egg
+# Se você quiser ver o Zen of Python original (em inglês) diretamente no seu interpretador Python, basta executar o comando abaixo:
+
+import this
