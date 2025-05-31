5e33ee1bf2cc5feb8887d1b9c42c1778bbce9d20
Rogerio Matos <rs_matos78@outlook.com>
2025-05-31 09:13:24 -0300

    aula 11 data e hora inicio

---
+++ Aula11 - Datas e Horas.py
@@ -0,0 +1,12 @@
+# Data e Hora é um objeto no Python
+# importando o metodo do modulo datetime
+# import datetime as dt # importando todas as funções da datetime
+
+from datetime import date, datetime, timedelta
+
+hoje = date.today()
+agora = datetime.now()
+
+print(hoje)
+print(agora)
+
